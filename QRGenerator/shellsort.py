ca0683a72c3613c7a17c266fdb16ce5e4d460d63
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-09 22:24:31 +0530

    shell sort

---
+++ QRGenerator/shellsort.py
@@ -0,0 +1,24 @@
+def shell_sort(arr):
+    size = len(arr)
+    gap = size // 2
+
+    while gap > 0:
+        for i in range(gap,size):
+            anchor = arr[i]
+            j = i
+            while j>=gap and arr[j-gap] > anchor:
+                arr[j] = arr[j-gap]
+                j -= gap
+                arr[j] = anchor
+        gap = gap // 2
+
+if __name__ == '__main__' :
+    tests = [
+        [21,29,9,4,10,45,90],
+        [4,9,32.23],
+        [7,50,65,5,15],
+        [],
+        ]
+    for elements in tests:
+      shell_sort(elements)    
+      print(elements)  
