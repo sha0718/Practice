94902bc44a1ad7905cc12c3b828fa00dd55527d0
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-10 10:47:51 +0530

    selection sort

---
+++ QRGenerator/selection.py
@@ -0,0 +1,23 @@
+def selection_sort(arr):
+    size = len(arr)
+    for i in range(size-1):
+        min_index = i
+        for j in range(min_index+1,size):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if i != min_index:
+            arr[i] ,arr[min_index] = arr[min_index],arr[i]
+
+
+if __name__ == '__main__':
+    tests = [
+        [14,65,78,45,35,23,90],
+        [13,15,18,16],
+        [],
+        [29,72,64,62],
+        [7,5]
+    ]
+    for elements in tests:
+      selection_sort(elements)  
+      print(elements)     
