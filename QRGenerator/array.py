72a81549ee1143d4e2b35abd22b253d5831fa08f
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-10 17:49:46 +0530

    object file

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,3 +1,34 @@
-import array as arr
-A = arr.array('i',[11,22,33,44,55])
-print(A[:])
+class Person:
+    def __init__(self, fname,lname):
+        self.fname = fname
+        self.lname = lname
+
+    def myfunc(self):
+       print(self.fname, self.lname )
+
+class Student(Person):
+    def __init__(self,fname,lname,year):
+        super().__init__(fname,lname)
+        self.graduation_year = year
+
+    def welcome(self):
+        print("Welcome",self.fname,self.lname,"to the class of", self.graduation_year)
+    
+p1 = Student("virat", "kohli",2023)
+p1.welcome()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
