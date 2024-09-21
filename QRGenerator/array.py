5e6cfa91b6f889ea3977ba9673f6ba5fc815d532
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-21 16:50:43 +0530

    set zeroes

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,19 +1,31 @@
-def rotate_image(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i+1,n):
-            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+def set_zeroes(matrix):
+    if not matrix or not matrix[0]:
+        return
+    
+    rows , cols = len(matrix) , len(matrix[0])
+    zero_rows = set()
+    zero_cols = set()
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:
+                zero_rows.add(i)
+                zero_cols .add(j)
 
-    for i in range(n):
-        matrix[i].reverse()
-image = [
-    [1, 2, 3],
-    [4, 5 ,6],
-    [7, 8, 9]
+    for i in zero_rows:
+        for j in range(cols):
+            matrix[i][j] = 0   
+
+    for j in zero_cols:
+        for i in range(rows):
+            matrix[i][j] = 0
+matrix = [
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
 ]            
-rotate_image(image) 
-for rows in image:
-    print(rows)   
+set_zeroes(matrix)
+for row in matrix:
+    print(row)
             
 
 
