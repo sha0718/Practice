de3f4dc021b9494cd52c836d2e4caecf930383b4
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-21 16:05:52 +0530

    rotate image

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,8 +1,19 @@
-def replace_spaces(input_string):
-    return input_string.replace(" ", '%20')
-result = replace_spaces("hello my name is virat")
-print(result) 
-    
+def rotate_image(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i+1,n):
+            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
+image = [
+    [1, 2, 3],
+    [4, 5 ,6],
+    [7, 8, 9]
+]            
+rotate_image(image) 
+for rows in image:
+    print(rows)   
             
 
 
