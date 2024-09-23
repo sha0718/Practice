8588b42f1129b5e49ad8dd2293025b8df10010eb
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-23 13:32:30 +0530

    fib series

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,13 +1,18 @@
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-num_terms = 20
-for i in range(num_terms):
-    print(fibonacci(i) , end = " ")
+    fib_sequence = [0, 1]  # Start with the first two numbers
+
+    # Generate the Fibonacci series until we reach n numbers
+    for i in range(2, n):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    return fib_sequence[:n]  # Return the first n numbers
+
+# Example usage
+num_terms = 10  # Change this to generate more or fewer terms
+print(fibonacci(num_terms))
+
+      
 
 
 
