8e2a7476b1de55fab36adaed4850c3795a8b3c9c
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-24 15:11:39 +0530

    quick

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,17 +1,19 @@
-def binary_search(sorted_list,target):
-    low = 0
-    high = len(sorted_list) - 1
-    while low <= high:
-        mid = low + high // 2
-        if sorted_list[mid] == target:
-            return mid
-        elif sorted_list[mid] < target:
-            low = mid + 1
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    less_than_pivot = []
+    greater_than_pivot = []
+    for item in arr[:-1]:
+        if item < pivot:
+            less_than_pivot.append(item)
         else:
-            high = mid - 1
-    return -1
-sorted_toys = [1,3,8,9,10,23]
-print(binary_search(sorted_toys ,23))
+            greater_than_pivot.append(item)
+    return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+array = [10,7,8,9,1,5]
+sorted_array = quick_sort(array)
+print("sorted array :",sorted_array)
+
 
 
 
