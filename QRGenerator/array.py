76af4cc46a41b2f2dcca9be393cde3bb6b8a642c
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-09-25 16:10:29 +0530

    radix

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,40 +1,37 @@
-def merge_sort(arr):
-    # Base case: if the array has 1 or 0 elements, it's already sorted
-    if len(arr) <= 1:
-        return arr
+def counting_sort(arr,exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
 
-    # Step 1: Divide the array into two halves
-    mid = len(arr) // 2
-    left_half = merge_sort(arr[:mid])  # Recursively sort the left half
-    right_half = merge_sort(arr[mid:])  # Recursively sort the right half
+    for i in range(1,10):
+        count[i] += count[i - 1] 
 
-    # Step 2: Merge the sorted halves
-    return merge(left_half, right_half)
+    for i in range(n-1,-1,-1):
+        index = (arr[i] // exp) % 10
+        output[count [index] - 1] = arr[i]
+        count[index] -= 1
 
-def merge(left, right):
-    sorted_array = []
-    i = j = 0
+    for i in range(n):
+        arr[i] = output[i]
+def radix_sort(arr):
+    max_num = max(arr) 
+    exp = 1
+    while max_num // exp > 0:
+        counting_sort(arr,exp)
+        exp *= 10
 
-    # Step 3: Merge the two sorted arrays
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            sorted_array.append(left[i])
-            i += 1
-        else:
-            sorted_array.append(right[j])
-            j += 1
-
-    # If there are remaining elements in left or right, add them
-    sorted_array.extend(left[i:])
-    sorted_array.extend(right[j:])
-
-    return sorted_array
-
-# Example usage
 if __name__ == "__main__":
-    array = [38, 27, 43, 3, 9, 82, 10]
-    sorted_array = merge_sort(array)
-    print("Sorted array:", sorted_array)
+    arr = [170,45,75,90,802,24,2,66]
+    print("original array :" , arr)
+    radix_sort(arr)
+    print("sorted array :" ,arr)                  
+
+
+           
+    
 
 
 
