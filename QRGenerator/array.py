174d89db7d7ccc9b905ff222a8835367f91adfaa
sha0718 <162859986+sha0718@users.noreply.github.com>
2024-10-05 15:47:20 +0530

    cartesian

--- QRGenerator/array.py
+++ QRGenerator/array.py
@@ -1,22 +1,26 @@
-def tower_of_hanoi(n,source,target, auxilary):
-    if n == 1:
-        print(f" move disk 1 from {source} to {target}")
-        return
-    tower_of_hanoi(n-1,source,auxilary,target)
-    print(f" move disk {n} from {source} to {target}")
-    tower_of_hanoi(n-1,auxilary,target,source)
-
-num_disk = 3
-tower_of_hanoi(num_disk,'A','B','C')    
-               
-
-
-
-           
+def cartesian_product(sets):
+    result = set()
     
+    def backtrack(index, path):
+        if index == len(sets):  # Base case: if we've selected from all sets
+            result.add(tuple(path))  # Add the current path as a tuple to the result set
+            return
+        for num in sets[index]:  # Iterate through the current set
+            path.append(num)  # Choose an element
+            backtrack(index + 1, path)  # Recur to the next set
+            path.pop()  # Backtrack to try the next element
+    
+    backtrack(0, [])
+    return result
+sets = [
+    [1, 2],
+    [3, 4],
+    [5, 6, 7]
+]
 
-
-
+result = cartesian_product(sets)
+for seq in result:
+    print(seq)
 
 
 
